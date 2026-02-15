05eff1f578097e97763d99ca20b09cb07ea30dd4
Miguel Murillo <brannmikel2004@gmail.com>
2026-02-15 18:01:14 -0500

    MINI-CICLO 3 Completo

---
+++ Lid.java
@@ -0,0 +1,19 @@
+
+/**
+ * Lid class, a lid can be paired with a cup.
+ * all lids are 1 cm height
+ * 
+ * @author (Murillo-Rubiano) 
+ * @version (1.0)
+ */
+public class Lid{
+    private int number;
+    private int height;
+    private String color;
+    private int xPosition;
+    private int yPosition;
+    private boolean isVisible;
+    private Cup pairedCup;
+    private Rectangle lidRectangle;
+    
+}
